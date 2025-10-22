91b3157b1adf635f7c21faced1b13059d76f992c
Rafael Nascimento Gomes <rafaelng2006@gmail.com>
2025-10-22 12:17:28 -0300

    Corrige teste de inicialização do Spring Boot

--- src/main/java/br/com/fiap/checkpoint5/Checkpoint5Application.java
+++ src/main/java/br/com/fiap/checkpoint5/Checkpoint5Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Checkpoint3Application {
+public class Checkpoint5Application {
     public static void main(String[] args) {
-        SpringApplication.run(Checkpoint3Application.class, args);
+        SpringApplication.run(Checkpoint5Application.class, args);
     }
 }
